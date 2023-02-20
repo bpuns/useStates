77993717fe971f477060d433681c15abf3930308
linjianhui <linjianhui12263@apexsoft.com.cn>
2023-02-20 19:58:06 +0800

    修改同步bug

--- src/useStates.ts
+++ src/useStates.ts
@@ -24,38 +24,43 @@ export function useStates<S extends Object>(defaultValue: S | (() => S)): UseSta
   // 派生状态
   const [ data, setData ] = useState<S>(defaultValue)
   // 存储当前hooks的一些状态
-  const store = useRef<UseStatesStore<S>>({ newData: undefined!, dirtyData: undefined })
+  const store = useRef<UseStatesStore<S>>({ newData: data, dirtyData: undefined })
 
   return [
-    useMemo(() => store.current.newData = data, [ data ]),
+    data,
     useCallback((
-      function (valueOrUpdateKey: any, newValueOrGetNewValue: any) {
+      function (fakeValueOrUpdateKey: any, fakeNewValue: any) {
         const storeValue = store.current
         const { newData, dirtyData } = storeValue
         const oldValue = dirtyData || newData
         // 判断是否是单节点更新
-        if (typeof valueOrUpdateKey === 'string') {
-          const newValue = isFunc(newValueOrGetNewValue) ? newValueOrGetNewValue(oldValue[valueOrUpdateKey]) : newValueOrGetNewValue
-          if (oldValue[valueOrUpdateKey] !== newValue) {
+        if (typeof fakeValueOrUpdateKey === 'string') {
+          const newValue = isFunc(fakeNewValue) ? fakeNewValue(oldValue[fakeValueOrUpdateKey]) : fakeNewValue
+          if (oldValue[fakeValueOrUpdateKey] !== newValue) {
             // 先放到脏数据中
-            storeValue.dirtyData = assign(oldValue, { [valueOrUpdateKey]: newValue })
+            storeValue.dirtyData = assign(oldValue, { [fakeValueOrUpdateKey]: newValue })
+          } else {
+            return
           }
         }
         // 批量更新
         else {
-          const nextState: Partial<S> = isFunc(valueOrUpdateKey) ? valueOrUpdateKey(oldValue) : valueOrUpdateKey
+          const nextState: Partial<S> = isFunc(fakeValueOrUpdateKey) ? fakeValueOrUpdateKey(oldValue) : fakeValueOrUpdateKey
           // 判断是否需要更新
           if (Object.keys(nextState).some(k => nextState[k] !== oldValue[k])) {
             storeValue.dirtyData = assign(oldValue, nextState)
+          } else {
+            return
           }
         }
         // 如果脏数据中没值，那么放到微任务队列中，等待宏任务执行完毕出发更新
-        if (!dirtyData){
-          queueMicrotask(()=>{
-            setData({ ...storeValue.dirtyData! })
+        if (!dirtyData) {
+          queueMicrotask(() => {
+            const dirtyData = storeValue.newData = { ...storeValue.dirtyData! }
             // 清空脏数据
             storeValue.dirtyData = undefined
-          }) 
+            setData(dirtyData)
+          })
         }
       }
     ), []) as UseStatesReturn<S>[1],
